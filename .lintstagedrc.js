c9eabaf799c20aa5838eb29691d67c03d994f313
duduluu <duduluu@outlook.com>
2020-02-20 21:46:48 +0800

    fix: add dist directory

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   '*.{js,jsx,mjs,ts,tsx}': 'eslint',
 
-  '*.css': 'stylelint',
   '*.scss': 'stylelint --syntax=scss',
 
   '*.{js,jsx,mjs,ts,tsx,json,yaml,css,scss,less,html,vue,graphql,gql,md}': 'prettier --write',
